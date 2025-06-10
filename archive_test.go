f1e9ea2cf6daffb85cca3f5da7c03a3c43efed6b
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2025-06-09 18:44:17 -0600

    Fix tests by resetting features and setting version

--- archive_test.go
+++ archive_test.go
@@ -81,10 +81,9 @@ func TestArchiveScenarios(t *testing.T) {
 	for _, ver := range []uint16{version1, version2} {
 		for _, tc := range cases {
 			t.Run(fmt.Sprintf("v%v_%s", ver, tc.name), func(t *testing.T) {
+				features = 0
 				if ver == version2 {
 					features.Set(fBlock)
-				} else {
-					features.Clear(fBlock)
 				}
 				version = ver
 
@@ -101,7 +100,7 @@ func TestArchiveScenarios(t *testing.T) {
 				archivePath = filepath.Join(tempDir, "test.goxa")
 				toStdOut = false
 				doForce = false
-				features = tc.createFlags | features
+				features |= tc.createFlags
 
 				cwd, _ := os.Getwd()
 				os.Chdir(tempDir)
@@ -112,7 +111,11 @@ func TestArchiveScenarios(t *testing.T) {
 				}
 
 				os.RemoveAll(root)
-				features = tc.extractFlags | features
+				features = 0
+				if ver == version2 {
+					features.Set(fBlock)
+				}
+				features |= tc.extractFlags
 
 				var dest string
 				if tc.extractFlags.IsSet(fAbsolutePaths) {
@@ -166,6 +169,7 @@ func TestArchiveParentRelative(t *testing.T) {
 
 	archivePath = filepath.Join(tempDir, "test.goxa")
 	features = 0
+	version = version1
 	toStdOut = false
 	doForce = false
 
@@ -209,6 +213,7 @@ func TestSymlinkAndHardlink(t *testing.T) {
 
 	archivePath = filepath.Join(tempDir, "test.goxa")
 	features = fSpecialFiles
+	version = version1
 	toStdOut = false
 	doForce = false
 
--- unicode_test.go
+++ unicode_test.go
@@ -23,6 +23,7 @@ func TestUnicodeFilenames(t *testing.T) {
 
 	archivePath = filepath.Join(tempDir, "test.goxa")
 	features = 0
+	version = version1
 	toStdOut = false
 	doForce = false
 
